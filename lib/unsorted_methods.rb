50e3bd3cabee52a179a0c28fdd1c8aa8d27b58d9
Philip Hale <phil989@gmail.com>
2014-05-07 12:18:59 +0100

    Remove dead code.

--- lib/unsorted_methods.rb
+++
@@ -1,8 +0,0 @@
-
-
-  def related
-    Rails.cache.fetch("#{self.id}-related") {
-      return [] if wordcounts.empty?
-      (Article.search(self.wordcounts.all(:order => 'count DESC', :limit => 10).map(&:keyword).map(&:name).join(" OR ")) - [self]).first(4)
-    }
-  end
