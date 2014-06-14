03b0a2cdd925eb6210117d7e046abebc6d1e14d3
Alan deLevie <adelevie@gmail.com>
2014-06-14 14:14:04 -0400

    Convert specs to RSpec 3.0.1 syntax with Transpec
    This conversion is done by Transpec 2.2.4 with the following command:
        transpec
    
    * 3 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/rails_nlp/keyword_spec.rb
+++ spec/rails_nlp/keyword_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 module RailsNlp
   describe Keyword do
-    it { should validate_presence_of(:name)}
-    it { should validate_uniqueness_of(:name)}
-    it { should have_many(:wordcounts) }
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_uniqueness_of(:name)}
+    it { is_expected.to have_many(:wordcounts) }
 
     describe "analysis of keywords before save" do
       describe "phonetic" do
